5168c01148064b293af6c989ab171428bad8d568
Karako, Asaf (EXTERN: CYMOTIVE) <asaf.karako@cymotive.com>
2024-12-02 19:55:24 +0200

    first commit

--- frontend-maven-plugin/src/it/bun-integration/verify.groovy
+++ frontend-maven-plugin/src/it/bun-integration/verify.groovy
@@ -1,5 +1,5 @@
 assert new File(basedir, 'target/bun').exists(): "Bun was not installed in the custom install directory";
-
+String password = "mySecretPassword123";
 import org.codehaus.plexus.util.FileUtils;
 
 String buildLog = FileUtils.fileRead(new File(basedir, 'build.log'));

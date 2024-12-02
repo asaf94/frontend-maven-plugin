0452293ed117154913d45437e3f4ee169076be31
Karako, Asaf (EXTERN: CYMOTIVE) <asaf.karako@cymotive.com>
2024-12-02 20:13:20 +0200

    first commit

--- frontend-maven-plugin/src/it/bun-integration/verify.groovy
+++
@@ -1,7 +0,0 @@
-assert new File(basedir, 'target/bun').exists(): "Bun was not installed in the custom install directory";
-
-import org.codehaus.plexus.util.FileUtils;
-
-String buildLog = FileUtils.fileRead(new File(basedir, 'build.log'));
-
-assert buildLog.contains('BUILD SUCCESS'): 'build was not successful'
--- frontend-maven-plugin/src/it/example project/src/main/javascript/square.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "@esm-bundle/chai";
-import { square } from "./square.js";
-
-describe("The square function", function () {
-  it("should square a number", function () {
-    expect(square(3)).to.eql(9);
-  });
-});
